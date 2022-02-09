70d31190a324feec36322a709ab8045fb124659a
nkavokine <nkavokine@flatironinstitute.org>
2022-02-09 15:29:41 -0500

    insert_segment cleanup

--- c++/triqs_ctseg/moves/insert_segment.cpp
+++ c++/triqs_ctseg/moves/insert_segment.cpp
@@ -7,69 +7,58 @@ namespace moves {
 
     SPDLOG_LOGGER_TRACE("\n =================== ATTEMPT INSERT ================ \n");
 
-    // --------  Selection of the times and color for the segment insertion
+    // ------------ Choice of segment --------------
 
-    // First case : there are segments. We pick up one segment, and insert at its right, i.e. smaller time
-
-    // pick up color and first time
+    // Select insertion color
     color = rng(data.n_color);
+    auto &sl       = config.seglist[color];
     SPDLOG_LOGGER_TRACE("Inserting at color {}", n_color);
 
-    // -------- Search for insertion window [tau1. tau2]
-
-    qmc_time_t tau1, tau2 = data.tau_seg.get_upper_pt();
-
-    bool config_is_empty = config.seglist[color].empty()
+    // Select insertion window [tau1,tau2] (defaults to [beta,0])
+    qmc_time_t tau1 = time_point_factory.get_upper_pt();
+    qmc_time_t tau2 = time_point_factory.get_lower_pt();
+    bool config_is_empty = sl.empty()
     if (not config_is_empty) {
-      // choose one segment randomly
-      auto &sl       = config.seglist[color];
-      long n_segment = rng(sl.size());
-      // tau1 : pos of cdag
-      // tau2 : pos of next c, possibly cyclic
-      tau1                 = sl[n_segment].tau_cdag;
-      bool is_last_segment = n_segment == sl.size() - 1;
-      tau2                 = sl[is_last_segment ? 0 : n_segment + 1].tau_c;
+      // Randomly choose one existing segment
+      long ind_segment = rng(sl.size());
+      tau1                 = sl[ind_segment].tau_cdag; // tau1 : pos of cdag
+      bool is_last_segment = ind_segment == sl.size() - 1;
+      tau2                 = sl[is_last_segment ? 0 : ind_segment + 1].tau_c; // tau2 : pos of next c, possibly cyclic
     }
 
-    // -------- Choose the new segment
-    //
+    // Choose new segment within insertion window
     auto l   = tau1 - tau2;
     auto dt1 = time_point_factory.get_random_pt(l);
     auto dt2 = time_point_factory.get_random_pt(l);
     if (dt1 == dt2) return 0;
-    if (dt1 < dt2) std::swap(dt1, dt2);
-
-    proposed_segment = segment_t{tau1 + dt1, tau1 + dt2};
-
+    if (dt1 > dt2) std::swap(dt1, dt2);
+    proposed_segment = segment_t{tau1 - dt1, tau1 - dt2};
     // The index of the segment if it is inserted in the list of segments.
-    proposed_segment_insert_pos = std::upper_bound(V.begin(), V.end(), comp);
+    proposed_segment_insert_pos = std::upper_bound(sl.begin(), sl.end(), proposed_segment);
 
     SPDLOG_LOGGER_TRACE("Inserting c at{}, cdag at {}", proposed_segment.tau_c, proposed_segment.tau_cdag);
 
-    // ----    compute ratio of dets  ------------
-
+    // ------------  Trace ratio  -------------
+        // FIXME : here we will need the K function integral 
     double ln_trace_ratio = 0;
-    for (auto c : range(seglists.size()))
+    for (int c : seglists) {
       if (c != color) ln_trace_ratio += overlap(proposed_segment, config.seglists[c]);
-
+    }
     double trace_ratio = std::exp(ln_trace_ratio);
 
-    // FIXME : here we will need the K function integral 
-
-    // ----    compute ratio det  ------------
+    // ------------  Det ratio  ---------------
 
     // Try insert in the det. We put all the segment in order in the dets.
     // Returns the ratio of dets (Cf det_manip doc).
     // pos is the index of the new segment, if inserted in the list.
     // FIXME : std::distance of an empoty?
     //long pos  = (config_is_empty ? 0 : std::distance(proposed_segment_insert_pos, V.begin()));
-    //
-    long pos  = std::distance(proposed_segment_insert_pos, V.begin());
+    long pos  = std::distance(proposed_segment_insert_pos, sl.begin());
     auto det_ratio = dets[color].try_insert(pos, pos, {proposed_segment.tau_c, 0}, {proposed_segment.tau_cdag, 0});
 
-    // ----    compute propos ratio ------------
+    // ------------  Proposition ratio ------------
 
-    double current_number_segments = config.seglist[color];
+    double current_number_segments = sl.size();
     double prop_ratio              = (current_number_segments + 1) / (current_number_segments * l * l);
 
     SPDLOG_LOGGER_TRACE("trace_ratio  = {}, prop_ratio = {}, det_ratio = {}", trace_ratio, prop_ratio, det_ratio);
@@ -84,10 +73,8 @@ namespace moves {
     SPDLOG_LOGGER_TRACE("\n - - - - - ====> ACCEPT - - - - - - - - - - -\n");
 
     data.dets[color].complete_operation();
-
-    // insert the segment in an ordered list
-    auto &V   = config.seglists[color];
-    V.insert(pos, proposed_segment);
+    // Insert the segment in an ordered list
+    config.seglists[color].insert(proposed_segment_insert_pos, proposed_segment);
 
     // FIXME ??? SIGNE ???
     double sign_ratio = 1; // ???config->trace.complete_insert_segment();
--- c++/triqs_ctseg/moves/insert_segment.hpp
+++ c++/triqs_ctseg/moves/insert_segment.hpp
@@ -5,33 +5,26 @@
 
 
 namespace moves {
-
   // FIXME : LIGNE PLEINE ?? dans overlap ???
-
-  /// Insert a segment $c_a(\tau)c_a^\dagger(\tau')$
-  //
   class insert_segment {
     param_t const &params;
     qmc_data const &data;
     configuration &config;
     triqs::mc_tools::random_generator &rng;
 
-    // internal data
-
+    // Internal data
     int color = 0;
     segment_t proposed_segment;
     std::vector<segment_t>>::iterator proposed_segment_insert_pos;
     time_point_factory_t time_point_factory = time_point_factory_t{params.beta};
 
     public:
-    // FIXME
-    //insert_segment(const qmc_data *data_, configuration *config_, triqs::mc_tools::random_generator &rng_)
-    //  : data(data_), config(config_), rng(rng_){};
-
-    // ----------------------------
-
+    // Constructor
+    insert_segment(const qmc_data *data_, configuration *config_, triqs::mc_tools::random_generator &rng_)
+      : data(data_), config(config_), rng(rng_){};
+    // ------------------
     double attempt();
     double accept();
     void reject();
   };
-} // namespace moves
+}; // namespace moves
