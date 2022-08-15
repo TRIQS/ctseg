a5c7f2c8e232261d68d25fcaab130376e3b59a16
nkavokine <nkavokine@flatironinstitute.org>
2022-08-15 09:34:32 -0400

    draft sperp measure with N^2 terms WARNING - doesn't work

--- c++/triqs_ctseg/measures/sperp_tau.cpp
+++ c++/triqs_ctseg/measures/sperp_tau.cpp
@@ -6,13 +6,13 @@ namespace measures {
   sperp_tau::sperp_tau(params_t const &p, work_data_t const &wdata, configuration_t const &config, results_t &results)
      : wdata{wdata}, config{config}, results{results} {
 
-    beta       = p.beta;
+    beta = p.beta;
 
     n_color = config.n_color();
 
-    ss_tau   = gf<imtime>({beta, Boson, p.n_tau_k}, {1,1});
+    ss_tau   = gf<imtime>({beta, Boson, p.n_tau_k}, {1, 1});
     ss_tau() = 0;
-    Z       = 0;
+    Z        = 0;
   }
 
   // -------------------------------------
@@ -23,11 +23,38 @@ namespace measures {
 
     Z += s;
 
-    for (auto const &[i, line] : itertools::enumerate(config.Jperp_list)) {
-        auto dtau = double(line.tau_Splus - line.tau_Sminus);
-        ss_tau[closest_mesh_pt(dtau)] += 1.0 / real(wdata.Jperp(dtau)(0,0));
+    for (auto const &[k, line] : itertools::enumerate(config.Jperp_list)) {
+      auto dtau1 = double(line.tau_Splus - line.tau_Sminus);
+      auto dtau2 = double(line.tau_Sminus - line.tau_Splus);
+      ss_tau[closest_mesh_pt(dtau1)] += 0.5 / (real(wdata.Jperp(dtau1)(0, 0)));
+      ss_tau[closest_mesh_pt(dtau2)] += 0.5 / (real(wdata.Jperp(dtau2)(0, 0)));
+    }
 
-    }  
+#if 0
+    auto N_lines = config.Jperp_list.size();
+
+    for (auto const &[i, line1] : itertools::enumerate(config.Jperp_list)) {
+      for (auto const &[j, line2] : itertools::enumerate(config.Jperp_list)) {
+        auto Jperp_list_loc         = config.Jperp_list;
+        Jperp_list_loc[i].tau_Splus = config.Jperp_list[j].tau_Splus;
+        Jperp_list_loc[j].tau_Splus = config.Jperp_list[i].tau_Splus;
+        auto t11                    = double(line1.tau_Splus - line1.tau_Sminus);
+        auto t22                    = double(line2.tau_Splus - line2.tau_Sminus);
+        auto t12                    = double(line1.tau_Splus - line2.tau_Sminus);
+        auto t21                    = double(line2.tau_Splus - line1.tau_Sminus);
+        auto w_mod                  = real(wdata.Jperp(t12)(0, 0)) * real(wdata.Jperp(t21)(0, 0))
+           / (real(wdata.Jperp(t11)(0, 0)) * real(wdata.Jperp(t22)(0, 0)));
+
+        for (auto const &[k, line] : itertools::enumerate(Jperp_list_loc)) {
+          auto dtau1 = double(line.tau_Splus - line.tau_Sminus);
+          auto dtau2 = double(line.tau_Sminus - line.tau_Splus);
+          ss_tau[closest_mesh_pt(dtau1)] += w_mod * 0.5 / (real(wdata.Jperp(dtau1)(0, 0)) * N_lines * N_lines);
+          ss_tau[closest_mesh_pt(dtau2)] += w_mod * 0.5 / (real(wdata.Jperp(dtau2)(0, 0)) * N_lines * N_lines);
+        }
+      }
+    }
+#endif
+
   }
 
   // -------------------------------------
@@ -37,7 +64,7 @@ namespace measures {
     Z = mpi::all_reduce(Z, c);
 
     ss_tau = mpi::all_reduce(ss_tau, c);
-    ss_tau = ss_tau / (- beta * Z * ss_tau.mesh().delta());
+    ss_tau = ss_tau / (-beta * Z * ss_tau.mesh().delta());
 
     // Fix the point at zero and beta
     ss_tau[0] *= 2;
